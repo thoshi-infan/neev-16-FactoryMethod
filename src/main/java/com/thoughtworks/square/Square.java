e004e5a4f786bb000bcf3393ffc50b6c17ba006d
thoshiba.infantina@gmail.com <thoshiba@Thoshibas-MacBook-Pro.local>
2022-03-29 13:20:13 +0530

    [Akshita/Thoshiba]  This commit will refactor the code to find area and  perimeter of a square

--- src/main/java/com/thoughtworks/square/Square.java
+++ src/main/java/com/thoughtworks/square/Square.java
@@ -3,19 +3,28 @@ package com.thoughtworks.square;
 public class Square {
 
     private final double side;
-    private final int perimeterValue = 4;
+    private final int perimeterValue;
+
+    {
+        perimeterValue = 4;
+    }
 
     public Square(double side) {
+
         this.side = side;
     }
 
     public double area() {
-        if(side <= 0) return 0;
+
+        if( side <= 0 )
+            return 0;
         return side * side;
     }
 
     public double perimeter() {
-        if(side <= 0) return 0;
+
+        if( side <= 0 )
+            return 0;
         return  perimeterValue * side;
     }
 
