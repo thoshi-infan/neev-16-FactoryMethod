7780ef581e267b45ea96e8b5a4eda9f032ca0c45
Thoshiba Infantina <thoshiba.infantina@thoughtworks.com>
2022-03-30 06:13:29 +0530

    Refactored the code

--- src/test/java/com/thoughtworks/square/SquareTest.java
+++ src/test/java/com/thoughtworks/square/SquareTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 
-public class SquareTest {
+public class SquareTest{
 
     @Test
     public void shouldReturnAreaWhenSideIsGiven(){
